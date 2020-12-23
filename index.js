a861dfb6ee1db3b15ce4a31222a14d6cbee3fb3e
hornegg <hornegg@users.noreply.github.com>
2020-12-23 12:08:25 +0000

    Matrix .getInverse() is being removed :-(

--- index.js
+++ index.js
@@ -135,7 +135,7 @@
     };
     ThreeBSP.prototype.toGeometry = function() {
       var i, j,
-        matrix = new THREE.Matrix4().getInverse( this.matrix ),
+        matrix = new THREE.Matrix4().copy( this.matrix ).invert(),
         geometry = new THREE.Geometry(),
         polygons = this.tree.allPolygons(),
         polygon_count = polygons.length,
